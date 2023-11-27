0a7c49d873b2d92777d6c041761a7bf7bf32c798
keithchad <keithchad6@gmail.com>
2023-11-27 15:24:40 +0300

    implement firebase sign in and sign up

--- app/src/main/java/com/chad/exams/SignIn.java
+++ app/src/main/java/com/chad/exams/SignIn.java
@@ -1,14 +1,80 @@
 package com.chad.exams;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class SignIn extends AppCompatActivity {
+
+    public EditText editTextEmail;
+    public EditText editTextPassword;
+    private FirebaseAuth firebaseAuth;
+    public MaterialButton buttonSignIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.signInButton);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        buttonSignIn.setOnClickListener(v -> implementFirebase());
+
     }
+
+    private void implementFirebase() {
+        String email = editTextEmail.getText().toString();
+        String password = editTextEmail.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                                .child( "Users").child(firebaseAuth.getCurrentUser().getUid());
+                        reference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                Intent intent = new Intent(SignIn.this, HomeActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(SignIn.this, ""+error, Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(e -> {
+
+                    Toast.makeText(SignIn.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                });
+
+    }
+
 }
--- app/src/main/java/com/chad/exams/SignUp.java
+++ app/src/main/java/com/chad/exams/SignUp.java
@@ -8,16 +8,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUp extends AppCompatActivity {
 
     public EditText editTextEmail;
     public EditText editTextPassword;
-
+    public EditText editTextName;
     public TextView textSignIn;
+    public MaterialButton buttonSignUp;
     public TextView textMain;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference reference;
 
 
     @SuppressLint("MissingInflatedId")
@@ -26,9 +36,14 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         editTextEmail = findViewById(R.id.editTextEmail);
+        editTextName = findViewById(R.id.editTextName);
         editTextPassword = findViewById(R.id.editTextPassword);
         textSignIn = findViewById(R.id.textSignIn);
         textMain = findViewById(R.id.textMain);
+        buttonSignUp = findViewById(R.id.signUpButton);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         textSignIn.setOnClickListener(v -> {
             Intent intent = new Intent(SignUp.this, SignIn.class);
@@ -39,15 +54,44 @@ public class SignUp extends AppCompatActivity {
         String mainName = intent.getStringExtra("Level");
         textMain.setText(mainName);
 
-        implementFirebase();
+        buttonSignUp.setOnClickListener(v -> {
+            implementFirebase();
+        });
+
     }
 
     private void implementFirebase() {
         String email = editTextEmail.getText().toString();
         String password = editTextEmail.getText().toString();
-    }
+        String name = editTextName.getText().toString();
 
-    String email = editTextEmail.getText().toString();
-    String password = editTextEmail.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                        String userId = null;
+                        if (firebaseUser != null) {
+                            userId = firebaseUser.getUid();
+                        }
+
+                        reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("Name", name);
+                        hashMap.put("Email", email);
+                        hashMap.put("UserId", userId);
+
+                        reference.setValue(hashMap).addOnCompleteListener(task1 -> {
+
+                            Intent intent = new Intent(SignUp.this, HomeActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        });
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "You can't register now. Try again later!", Toast.LENGTH_SHORT).show();
+                });
+    }
 
 }
