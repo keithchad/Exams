abb724dd5913c72116659fb71b4d923a611ac88d
keithchad <keithchad6@gmail.com>
2023-11-29 14:18:39 +0300

    admin changes

--- app/src/main/java/com/chad/exams/HomeActivityAdmin.java
+++ app/src/main/java/com/chad/exams/HomeActivityAdmin.java
@@ -1,14 +1,64 @@
 package com.chad.exams;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivityAdmin extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private StudentsAdapter studentsAdapter;
+    private List<Student> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView = findViewById(R.id.adminRecyclerView);
+
+        list = new ArrayList<>();
+        studentsAdapter = new StudentsAdapter(getApplicationContext(), list);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(studentsAdapter);
+
+        readStudents();
+    }
+
+    private void readStudents() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Student");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Student students = dataSnapshot.getValue(Student.class);
+                    list.add(students);
+                }
+                studentsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
